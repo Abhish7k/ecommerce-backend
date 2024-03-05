5ad3590a915e27d26e0bafb39caaad1be6ab5db9
Abhishek <abhishekbhosale.dev@gmail.com>
2024-03-06 00:08:27 +0530

    feat: dotenv config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+const { PORT } = require("./config/serverConfig.js");
+
+const app = express();
+
 app.listen(PORT, () => {
   console.log("server for shopkart is up");
 });
